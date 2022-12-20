0ab79bd2341026da88aaa916727c54f655692f92
Gregory Ecock <gecock@us.ibm.com>
2022-12-20 10:52:41 -0500

    experiments config params, output files
    Signed-off-by: Gregory Ecock <gecock@us.ibm.com>

--- experiment.py
+++ experiment.py
@@ -28,42 +28,47 @@ class Experiments:
             for weight in weight_values:
                 for sds in sds_values:
                     for bas in bas_values:
-                        counter=0
                         bias = round(bias,1)
                         weight = round(weight, 1)
                         print(bias, weight, sds, bas)
 
-                        experiment_output_dir = self.output_dir + "/" + bias + "_" + weight + "_" + sds + "_" + bas
+                        experiment_output_dir = self.output_dir + "/" + str(bias) + "_" + str(weight) + "_" + str(sds) + "_" + str(bas)
                         os.makedirs(experiment_output_dir, exist_ok=True)
 
-                        exp_config_path = experiment_output_dir + "/" + self.config
-                        copyfile(self.config, exp_config_path)
+                        exp_config_path = experiment_output_dir + "/" + self.config.config_file
+                        copyfile(self.config.config_file, exp_config_path)
 
                         #Update config settings for the experiment
                         exp_config = Config(exp_config_path)
 
                         file_info = os.path.split(exp_config.getValue('ErrorRateOutput', 'details_file'))
-                        details_file = os.path.join(file_info[0], experiment_output_dir, file_info[1])
+                        details_file = os.path.join(experiment_output_dir, file_info[1])
                         exp_config.setValue('ErrorRateOutput', 'details_file', details_file)
 
                         file_info = os.path.split(exp_config.getValue('ErrorRateOutput', 'summary_file'))
-                        details_file = os.path.join(file_info[0], experiment_output_dir, file_info[1])
-                        exp_config.setValue('ErrorRateOutput', 'summary_file', details_file)
+                        summary_file = os.path.join(experiment_output_dir, file_info[1])
+                        exp_config.setValue('ErrorRateOutput', 'summary_file', summary_file)
 
                         file_info = os.path.split(exp_config.getValue('ErrorRateOutput', 'word_accuracy_file'))
-                        details_file = os.path.join(file_info[0], experiment_output_dir, file_info[1])
-                        exp_config.setValue('ErrorRateOutput', 'word_accuracy_file', details_file)
+                        word_accuracy_file = os.path.join(experiment_output_dir, file_info[1])
+                        exp_config.setValue('ErrorRateOutput', 'word_accuracy_file', word_accuracy_file)
 
                         file_info = os.path.split(exp_config.getValue('Transcriptions', 'stt_transcriptions_file'))
-                        details_file = os.path.join(file_info[0], experiment_output_dir, file_info[1])
-                        exp_config.setValue('Transcriptions', 'stt_transcriptions_file', details_file)
-                                                
-                        exp_config.setValue('SpeechToText', "max_threads", int(max_threads))
+                        stt_transcriptions_file = os.path.join(experiment_output_dir, file_info[1])
+                        exp_config.setValue('Transcriptions', 'stt_transcriptions_file', stt_transcriptions_file)
 
-                        exp_config.setValue('SpeechToText', "speech_detector_sensitivity", float(sds))
-                        exp_config.setValue('SpeechToText', "background_audio_suppression", float(bas))
-                        exp_config.setValue('SpeechToText', "character_insertion_bias", float(bias))
-                        exp_config.setValue('SpeechToText', "customization_weight", float(weight))
+                        file_info = os.path.split(exp_config.getValue('ErrorRateOutput', 'stt_transcriptions_file'))
+                        stt_transcriptions_file = os.path.join(experiment_output_dir, file_info[1])
+                        exp_config.setValue('ErrorRateOutput', 'stt_transcriptions_file', stt_transcriptions_file)
+                                                
+                        exp_config.setValue('SpeechToText', "max_threads", str(max_threads))
+
+                        exp_config.setValue('SpeechToText', "speech_detector_sensitivity", str(sds))
+                        exp_config.setValue('SpeechToText', "background_audio_suppression", str(bas))
+                        exp_config.setValue('SpeechToText', "character_insertion_bias", str(bias))
+                        exp_config.setValue('SpeechToText', "customization_weight", str(weight))
+
+                        exp_config.writeFile(exp_config_path)
 
                         #Get Transcriptions 
                         transcribe.run(exp_config_path)
@@ -71,7 +76,9 @@ class Experiments:
                         #Get Analysis
                         analyze.run(exp_config_path)
 
-    def run_report(output_dir, config):
+
+
+    def run_report(self, output_dir, config):
         print(f"Reporting from {output_dir}")
 
         # Extract all summaries
@@ -117,30 +124,29 @@ def main():
     # build generators
     experiments = Experiments(config, output_dir)
     max_threads = int(config.getValue("SpeechToText","max_threads", 1))
-    sds_min  = float(config.getValue("SpeechToText", "sds_min"))
-    sds_max  = float(config.getValue("SpeechToText", "sds_max"))
-    sds_step  = float(config.getValue("SpeechToText", "sds_step"))
-    bias_min  = float(config.getValue("SpeechToText", "bias_min"))
-    bias_max  = float(config.getValue("SpeechToText", "bias_max"))
-    bias_step  = float(config.getValue("SpeechToText", "bias_step"))
-    cust_weight_min  = float(config.getValue("SpeechToText", "cust_weight_min"))
-    cust_weight_max  = float(config.getValue("SpeechToText", "cust_weight_max"))
-    cust_weight_step  = float(config.getValue("SpeechToText", "cust_weight_step"))
-    bas_min = float(config.getValue("SpeechToText", "bas_min"))
-    bas_max = float(config.getValue("SpeechToText", "bas_max"))
-    bas_step = float(config.getValue("SpeechToText", "bas_step"))
+    sds_min  = float(config.getValue("Experiments", "sds_min"))
+    sds_max  = float(config.getValue("Experiments", "sds_max"))
+    sds_step  = float(config.getValue("Experiments", "sds_step"))
+    bias_min  = float(config.getValue("Experiments", "bias_min"))
+    bias_max  = float(config.getValue("Experiments", "bias_max"))
+    bias_step  = float(config.getValue("Experiments", "bias_step"))
+    cust_weight_min  = float(config.getValue("Experiments", "cust_weight_min"))
+    cust_weight_max  = float(config.getValue("Experiments", "cust_weight_max"))
+    cust_weight_step  = float(config.getValue("Experiments", "cust_weight_step"))
+    bas_min = float(config.getValue("Experiments", "bas_min"))
+    bas_max = float(config.getValue("Experiments", "bas_max"))
+    bas_step = float(config.getValue("Experiments", "bas_step"))
 
-    base_model_name = str(config.getValue("SpeechToText", "base_model_name"))
     custom_model = str(config.getValue("SpeechToText", "language_model_id"))
     
-    bias_range = drange(bias_min, bias_max+bias_step, bias_step) if base_model_name.find('bandModel') == -1 else drange(0.0, 0.1, 0.1)
+    bias_range = drange(bias_min, bias_max+bias_step, bias_step)
     weight_range = drange(cust_weight_min, cust_weight_max+cust_weight_step, cust_weight_step) if custom_model else drange(0.0, 0.1, 0.1)
     sds_range = drange(sds_min, sds_max+sds_step, sds_step) 
     bas_range = drange(bas_min, bas_max+bas_step, bas_step)
     
     experiments.run_all_experiments(bias_range, weight_range, sds_range, bas_range, max_threads)
 
-    run_report(output_dir, config)
+    experiments.run_report(output_dir, config)
 
 if __name__ == '__main__':
     main()
